312dfad06c2153311b79291c131963bec3eb3d8d
aleksander Micyk <aleksander.micyk@gmail.com>
2022-10-03 23:37:35 +0200

    add active button and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
-import {BrowserRouter as Router, Link,Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Link, NavLink, Routes, Route} from 'react-router-dom'
 
 
 const Home = () => <h1>Strona startowa</h1>
 const News = () => <h1>Aktualności</h1>
 const Contact = () => <h1>Kontakt do nas</h1>
+
+const ErrorPage = () => <h1>Strona nie istnieje</h1>
 
 class App extends Component {
   render(){
@@ -18,9 +20,9 @@ class App extends Component {
                 {/* <li><a href="/">Start</a></li>
                 <li><a href="/news">Aktualności</a></li>
                 <li><a href="/contact">Kontakt</a></li> */}
-                <li><Link to="/">Start</Link></li>
-                <li><Link to="/news">Aktualności</Link></li>
-                <li><Link to="/contact">Kontakt</Link></li>
+                <li><NavLink to="/" end>Start</NavLink></li>
+                <li><NavLink to="/news">Aktualności</NavLink></li>
+                <li><NavLink to="/contact">Kontakt</NavLink></li>
               </ul>
             </nav>
           </header>
@@ -29,6 +31,7 @@ class App extends Component {
               <Route path="/" element={<Home/>}/>
               <Route path="/news" element={<News/>}/>
               <Route path="/contact" element={<Contact/>}/>
+              <Route path="*" element={<ErrorPage/>}/>
             </Routes>
           </section>
         </div>
